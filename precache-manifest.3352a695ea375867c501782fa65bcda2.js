105f2b2265b584bef06fb11d244638189b60a9b5
Shai Michaeli <shaiws2@gmail.com>
2019-01-30 13:55:22 +0200

    Updates

--- precache-manifest.3352a695ea375867c501782fa65bcda2.js
+++ precache-manifest.3352a695ea375867c501782fa65bcda2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/bit.ly-clicks-counter/static/js/runtime~main.927cc5c6.js"
   },
   {
-    "revision": "2c1b4960b577e7c71db0",
-    "url": "/bit.ly-clicks-counter/static/js/main.2c1b4960.chunk.js"
+    "revision": "da1240126ae4e25f3614",
+    "url": "/bit.ly-clicks-counter/static/js/main.da124012.chunk.js"
   },
   {
     "revision": "5dcbecee6739915cbd6f",
     "url": "/bit.ly-clicks-counter/static/js/1.5dcbecee.chunk.js"
   },
   {
-    "revision": "2c1b4960b577e7c71db0",
+    "revision": "da1240126ae4e25f3614",
     "url": "/bit.ly-clicks-counter/static/css/main.848c7831.chunk.css"
   },
   {
-    "revision": "ef47b3bab55fa7057f46a26243f12dbd",
+    "revision": "7db257bfb94948502247d88a44575d38",
     "url": "/bit.ly-clicks-counter/index.html"
   }
 ];
