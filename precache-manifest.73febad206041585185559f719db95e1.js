cec4d60ed7afd95e6537ffcaf746f220e0e85c73
Shai Michaeli <shaiws2@gmail.com>
2019-01-30 13:19:26 +0200

    Updates

---
+++ precache-manifest.73febad206041585185559f719db95e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "927cc5c6d23b55a75b64",
+    "url": "/bit.ly-clicks-counter/static/js/runtime~main.927cc5c6.js"
+  },
+  {
+    "revision": "42133d09e2bbc6e67a8f",
+    "url": "/bit.ly-clicks-counter/static/js/main.42133d09.chunk.js"
+  },
+  {
+    "revision": "5dcbecee6739915cbd6f",
+    "url": "/bit.ly-clicks-counter/static/js/1.5dcbecee.chunk.js"
+  },
+  {
+    "revision": "42133d09e2bbc6e67a8f",
+    "url": "/bit.ly-clicks-counter/static/css/main.848c7831.chunk.css"
+  },
+  {
+    "revision": "4e4eaa8050b958b300312476a9e361ac",
+    "url": "/bit.ly-clicks-counter/index.html"
+  }
+];
